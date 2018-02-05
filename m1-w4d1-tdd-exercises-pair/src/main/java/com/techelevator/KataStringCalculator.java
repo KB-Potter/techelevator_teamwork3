5173d107085ee382d520ad8bb2e71e239e1ef6f0
Katie <katie.beatrice.potter@gmail.com>
2018-02-05 16:17:17 -0500

    Finsihed Calculator for now

--- m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
+++ m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
@@ -1,5 +1,20 @@
 package com.techelevator;
 
 public class KataStringCalculator {
-
+	
+	public int add(String numbers) {
+		
+		int result = 0; 
+		
+		if (!numbers.isEmpty()) {
+			
+			String[] temp =  numbers.split(",|\n|;|!"); 
+			
+			for (String s: temp) {
+				int number = Integer.parseInt(s); 
+				result += number; 
+		}
+	}
+		return result; 
+	}
 }
