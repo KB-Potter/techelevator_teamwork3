2bf134060f1ff55283a1aa8256cd88dc7446fb88
Shana Girard <Rochelle1@gmx.de>
2018-02-09 09:55:08 -0500

    committing changes in order to get the vending machine

--- m1-w4d2-file-io-part1-exercises-pair/src/main/java/com/techelevator/WordCount.java
+++ m1-w4d2-file-io-part1-exercises-pair/src/main/java/com/techelevator/WordCount.java
@@ -2,12 +2,43 @@ package com.techelevator;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
 public class WordCount {
-
-	public static void main(String[] args) {
+		
+		public static void main(String[] args) throws FileNotFoundException {
+					
+				File alice = new File("/Users/kpotter/Development/java-week4-pair-exercise/m1-w4d2-file-io-part1-exercises-pair/alices_adventures_in_wonderland.txt"); 
+					
+					int wordCount = 0; 
+					int sentenceCount = 0;
+					
+					
+			try(Scanner fileScanner = new Scanner(alice)) {
+						
+						
+						while (fileScanner.hasNextLine()) {
+							String wordLine = fileScanner.nextLine(); 
+							
+							if (wordLine.isEmpty()) {
+								continue;
+							}
+							
+							String[] temp = wordLine.trim().split("\\s+"); 
+							wordCount += temp.length; 
+							String[] temp2 = wordLine.split("[!.?]");
+							
+							sentenceCount += temp2.length - 1; 
+							
+						}
+						
+						
+					}
+					
+					System.out.println("The word count is: " + wordCount );
+					System.out.println("The sentence count is: " + sentenceCount);
 
 	}
 
--- m1-w4d3-file-io-part2-exercises-pair/src/main/java/com/techelevator/FindAndReplace.java
+++ m1-w4d3-file-io-part2-exercises-pair/src/main/java/com/techelevator/FindAndReplace.java
@@ -2,26 +2,53 @@ package com.techelevator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FindAndReplace {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		
 		File alice = new File("/Users/sgirard/workspace/pair-exercises/java-week4-pair-exercise/m1-w4d3-file-io-part2-exercises-pair/alices_adventures_in_wonderland.txt"); 
 			
 			Scanner in = new Scanner(System.in);
+			System.out.println("Please enter the complete file path of the origin file you'd like to search:");
+			File originFile = getInputFileFromUser();
 			
 			//setup destination file
-			String basepath = "/Users/sgirard/workspace/pair-exercises/java-week4-pair-exercise/m1-w4d3-file-io-part2-exercises-pair";
-			String fakeAlice = basepath + "/newAlice.txt";
-			File newAlice = new File(fakeAlice);
+			System.out.println("Please enter a destination path:");
+			String destinationPath = in.nextLine();
 			
-			if (newAlice.exists() == false) {
+			File destinationFile = new File(destinationPath);
+			
+			try {
+				String fakeAlice = destinationPath + "newAlice.txt";
+				File newAlice = new File(fakeAlice);
+			} catch (FileNotFoundException e) {
+				System.out.println("Invalid destination path");
+				break;
+			}
+			
+			
+			
+			
+//			String basepath = "/Users/sgirard/workspace/pair-exercises/java-week4-pair-exercise/m1-w4d3-file-io-part2-exercises-pair";
+//			String fakeAlice = basepath + "/newAlice.txt";
+//			File newAlice = new File(fakeAlice);
+			
+			if (originFile.exists() == false) {
 				try {
-					newAlice.createNewFile();
+					originFile.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if (destinationFile.exists() == false) {
+				try {
+					destinationFile.createNewFile();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -33,36 +60,44 @@ public class FindAndReplace {
 			System.out.println("Which word are we replacing it with?");
 			String replaceWord = in.next();
 			
-			try(Scanner fileScanner = new Scanner(alice)) {
-				while (fileScanner.hasNextLine()) {
-					String wordLine = fileScanner.nextLine(); 
-					
-					
-					String[] temp = wordLine.split(searchWord); 
-					
-					//String replaced = "";
-					//for (int i = 0; i < temp.length; i++) {
-					
-					String replaced = temp[0] + replaceWord.trim();// + temp[1];
-					
-					try (PrintWriter writer = new PrintWriter(newAlice)) {//it's not appeding it's overwriting. open printwriter first and open scanner inside of it. open printwriter as a filewriter and it will append if true. We HAVE to append it
-						
-						if (wordLine.contains(searchWord)) {
-							writer.println(replaced);
-						} else {
-							writer.print(wordLine);
-						}
-						writer.flush();
-						}
-					}
-					
-				}
-				if (alice.getTotalSpace() ==	newAlice.getTotalSpace()) {
-							System.out.println("Search word not found!");
+//			try(Scanner fileScanner = new Scanner(alice)) {
+//				while (fileScanner.hasNextLine()) {
+//					String wordLine = fileScanner.nextLine(); 
+//					
+//					String replaced = wordLine.replaceAll(searchWord, replaceWord);
+//					
+//					try (FileWriter writer = new FileWriter(newAlice, true)) {
+//						if (wordLine.contains(searchWord)) {
+//							writer.write(replaced + "\n");
+//						} else {
+//							writer.write(wordLine + "\n");
+//						}
+//						writer.flush();
+//						}
+//					}
+//					
+//				}
+//				if (alice.getTotalSpace() ==	newAlice.getTotalSpace()) {
+//							System.out.println("Search word not found!");
 				
 			}
 			
-
+	@SuppressWarnings("resource")
+	private static File getInputFileFromUser() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Please enter path to input file >>> ");
+		String path = userInput.nextLine();
+		
+		File inputFile = new File(path);
+		if(inputFile.exists() == false) { // checks for the existence of a file
+			System.out.println(path+" does not exist");
+			System.exit(1); // Ends the program
+		} else if(inputFile.isFile() == false) {
+			System.out.println(path+" is not a file");
+			System.exit(1); // Ends the program
+		}
+		return inputFile;
+	}
 }
 
-}
+
